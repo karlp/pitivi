c0333e098e989373738364d0ae6c0991ae54165d
Thibault Saunier <tsaunier@src.gnome.org>
2010-05-24 11:37:21 -0400

    Make a single implementation of EffectFactory instead of the AudioEffectFactory and VideoEffectFactory

--- pitivi/timeline/track.py
+++ pitivi/timeline/track.py
@@ -607,7 +607,6 @@ class TrackObject(Signallable, Loggable):
         if self.gnl_object is None:
             raise TrackError()
 
-        #print "SELF.FACTORY: %s %s" % (self.factory, self.stream)
         bin = self.factory.makeBin(self.stream)
         self.gnl_object.add(bin)
         self._rebuild_interpolators = True

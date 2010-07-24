08cb58316dea44dbc0f038e962714358e260cce1
Thibault Saunier <tsaunier@gnome.org>
2010-07-23 23:29:19 -0400

    Add the active property to TrackObject

--- pitivi/timeline/track.py
+++ pitivi/timeline/track.py
@@ -304,6 +304,7 @@ class TrackObject(Signallable, Loggable):
         'priority-changed': ['priority'],
         'selected-changed' : ['state'],
         'stagger-changed' : ['stagger'],
+        'active-changed' : ['active'],
     }
 
     def __init__(self, factory, stream, start=0,
@@ -454,6 +455,14 @@ class TrackObject(Signallable, Loggable):
             self.gnl_object.props.start = position
 
     start = property(_getStart, setStart)
+
+    def _getActive(self):
+        return self.gnl_object.props.active
+
+    def setActive(self, active):
+        self.gnl_object.props.active = active
+
+    active = property(_getActive, setActive)
 
     def _getDuration(self):
         return self.gnl_object.props.duration
@@ -662,6 +671,9 @@ class TrackObject(Signallable, Loggable):
             self._public_priority = public_priority
             self.emit('priority-changed', public_priority)
 
+    def _notifyActiveCb(self, obj, pspec):
+        self.emit('active-changed', obj.props.active)
+
     def _connectToSignals(self, gnl_object):
         gnl_object.connect('notify::start', self._notifyStartCb)
         gnl_object.connect('notify::duration', self._notifyDurationCb)
@@ -672,6 +684,8 @@ class TrackObject(Signallable, Loggable):
                 self._notifyMediaStopCb)
         gnl_object.connect('notify::priority',
                 self._notifyPriorityCb)
+        gnl_object.connect('notify::active',
+                self._notifyActiveCb)
 
     def _disconnectFromSignals(self):
         if self.gnl_object:
